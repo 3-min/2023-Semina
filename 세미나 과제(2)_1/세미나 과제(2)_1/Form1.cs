129c2d81854d3cdd8ce9358830fc09f99e1d008b
3-min <Glaemfdj@oasis.inje.ac.kr>
2023-05-18 23:47:54 +0900

    2023-1학기 세미나 과제

---
+++ 세미나 과제(2)_1/세미나 과제(2)_1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 세미나_과제_2__1
+{
+    public partial class Form1 : Form
+    {
+        int[] A = new int[10];
+
+        System.Random Rannum = new System.Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                for(int i = 0; i < 10; i++)
+                {
+                    A[i] = Rannum.Next(1, 100);
+                    label1.Text += A[i] + ", ";
+                }
+                
+            }
+            catch(Exception ex)
+            {
+                label1.Text = ex.Message;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int max = A.Max();
+                label2.Text = "가장 큰 수 : " + max;
+            }
+            catch(Exception ex)
+            {
+                label2.Text = ex.Message;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Array.Sort(A);
+                label1.Text = String.Join(", ", A);
+            }
+            catch(Exception ex)
+            {
+                label2.Text = ex.Message;
+            }
+        }
+    }
+}
---
+++ 세미나 과제(2)_2/세미나 과제(2)_2/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 세미나_과제_2__2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int num = int.Parse(textBox1.Text);
+
+                label1.Text = "";
+                for(int i = 0; i <= num; i++)
+                {
+                    for(int y = 0; y <= i; y++)
+                    {
+                        label1.Text += "*";
+                    }
+                    label1.Text += "\n";
+                }
+            }
+            catch (Exception ex)
+            {
+                label1.Text = ex.Message;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int num = int.Parse(textBox1.Text);
+                int i, y, k, w;
+
+                label2.Text = "";
+                for(i = 1; i <= num; i++)
+                {
+                    for(y = 1; y <= num - i; y++)
+                    {
+                        label2.Text += " ";
+                    }
+                    for(k = 1; k <= 2 * i - 1; k++)
+                    {
+                        label2.Text += "*";
+                    }
+                    label2.Text += "\n";
+                }
+            }
+            catch(Exception ex)
+            {
+                label2.Text = ex.Message;
+            }
+        }
+    }
+}
